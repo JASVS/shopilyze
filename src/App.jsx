8231f3f00c91d514b7d03c2dd6b6de7f4e0d8a4b
ura1722 <birkovych.yurii@student.uzhnu.edu.ua>
2024-11-08 20:21:12 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Admin, Resource } from 'react-admin';
+import dataProvider from './providers/dataProvider';
+import { ProductList, ProductEdit, ProductCreate } from './products';
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+      <Resource name="products" list={ProductList} edit={ProductEdit} create={ProductCreate} />
+  </Admin>
+);
+
+export default App;
